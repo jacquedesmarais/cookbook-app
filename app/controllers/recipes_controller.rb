be98a17d892bf98e9a78d07574145f1e20efbba5
“jacquedesmaraisgit config --global user.email “jacquedes@gmail.com <jacquedes@gmail.com>
2017-08-29 19:37:52 -0500

    added gems

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,4 +6,10 @@ def one_recipe_action
   render 'one_recipe_page.html.erb'
 end
 
+def all_recipes_action
+  @title = "All Recipe"
+  @recipes = Recipe.all
+  render 'all_recipes_page.html.erb'
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get '/one_recipe_url' => 'recipes#one_recipe_action'
+  get 'all_recipes_url' => 'recipes#all_recipes_action'
 end
